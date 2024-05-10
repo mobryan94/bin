9a691d10569201142808bd906230b752aedfd391
0neFrame <chup.jr@gmail.com>
2024-05-10 10:52:05 +0300

    hot-fix 2

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fake-binance/",
+  base: "/",
 
   assetsInclude: ["**.PNG", "**.SVG"],
 
